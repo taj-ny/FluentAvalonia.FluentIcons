1760d8ae9eed65bd00330a75504afdedfba197f2
Taj_ny <79316397+taj-ny@users.noreply.github.com>
2022-11-06 07:33:24 +0100

    Add sample app project

---
+++ FluentAvalonia.FluentIcons.SampleApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FluentAvalonia.FluentIcons.SampleApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
